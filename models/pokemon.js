3b62b8a29e0fc03bc7cb9e1dcdd14cb8d4c86602
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-11 02:33:40 -0700

    wip: update models: added (required)

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const pokemonSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    type: String,
-    level: {type: Number, min: 1, max: 100, require: true},
-    caught: {type: Boolean, default: true}
+    name: {
+        type: String, 
+        required: true,
+    },
+    type: {
+        type: String, 
+        required: true,
+    },
+    level: {
+        type: Number, 
+        min: 1, 
+        max: 100, 
+        required: true,
+    },
+    caught: {
+        type: Boolean, 
+        default: true, 
+    }
 });
 
 const Pokemon = mongoose.model('Pokemon', pokemonSchema);
