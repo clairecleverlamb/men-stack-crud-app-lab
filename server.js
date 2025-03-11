04b4c5bcc1f21cad531e2989e8b8d8007104747c
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-11 03:47:23 -0700

    adding styling

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+app.use(express.static('public'));
 // const fetch = require('node-fetch');
 
 const Pokemon = require('./models/pokemon.js');
@@ -27,6 +28,40 @@ app.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+
+//-----------------fetch Pokemon from PokeAPI-------------
+
+// async function fetchPokemon() {
+//     try {
+//         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+//         const data = await response.json();
+//         const pokemonList = await Promise.all(
+//             data.results.map(async (poke) => {
+//                 const res = await fetch(poke.url);
+//                 const details = await res.json();
+//                 return {
+//                     id: details.id,
+//                     name: details.name,
+//                     type: details.types[0].type.name,
+//                     level: details.stats[0].base_stat 
+//                 };
+//             })
+//         );
+//         return pokemonList;
+//     } catch (error) {
+//         console.error('Error fetching Pokémon:', error);
+//         return [];
+//     }
+// }
+
+// -----------Show API Pokémon and custom Pokémon -----------------
+// app.get('/pokemons', async (req, res) => {
+//     const apiPokemon = await fetchPokemon();
+//     const customPokemon = await Pokemon.find();
+//     res.render('pokemons/index.ejs', { apiPokemon, customPokemon });
+// });
+
+
 //------------Catch new pokemon-----------------
 app.get('/pokemons/new', (req, res) => {
     res.render('pokemons/new.ejs');
@@ -40,13 +75,13 @@ app.post('/pokemons', async (req, res) => {
 
 app.get('/pokemons', async (req, res) => {
     const allPokemons = await Pokemon.find();
-    res.render('pokemons/index.ejs', { pokemons:allPokemons });
+    res.render('pokemons/index.ejs', { pokemons: allPokemons });
 })
 
 //-------------Show the pokemon---------------------
 app.get('/pokemons/:pokemonId', async (req, res) => {
     let pokemon;
-    if (mongoose.Types.ObjectId.isValid(req.params.pokemonId)){
+    if (mongoose.Types.ObjectId.isValid(req.params.pokemonId)) {
         pokemon = await Pokemon.findById(req.params.pokemonId);
         if (pokemon) {
             pokemon.isCustom = true;
@@ -66,17 +101,17 @@ app.get('/pokemons/:pokemonId', async (req, res) => {
 app.delete('/pokemons/:pokemonId', async (req, res) => {
     await Pokemon.findByIdAndDelete(req.params.pokemonId);
     res.redirect('/pokemons');
-  });
+});
 
 // --------------Edit the Pokemon Form -----------------
 
 app.get('/pokemons/:pokemonId/edit', async (req, res) => {
     const foundPokemon = await Pokemon.findById(req.params.pokemonId);
     if (!foundPokemon) {
-      return res.status(404).send('Pokémon not found');
+        return res.status(404).send('Pokémon not found');
     }
     res.render('pokemons/edit.ejs', { pokemon: foundPokemon });
-  });
+});
 
 //--------------- Update Pokemon ------------------------
 
