2e43f97d74839df1eac9f69ce6f55dbeb250dbbf
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-11 01:41:58 -0700

    wip: added properties

--- models/pokemon.js
+++ models/pokemon.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const pokemonSchema = new mongoose.Schema({
     name: {type: String, require: true},
     type: String,
-    abilities: [String],
-    imageUrl: String
+    level: {type: Number, min: 1, max: 100, require: true},
+    caught: {type: Boolean, default: true}
 });
 
 const Pokemon = mongoose.model('Pokemon', pokemonSchema);
